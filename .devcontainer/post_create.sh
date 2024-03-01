c45f53e948c9b32e8e11fce3c6053d6340d9898c
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-03-01 07:47:48 -0600

    devcontainer changes

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,28 @@
+yarn install --network-timeout 180000
+yarn electron
+
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+
+cd WPILib_Linux-2024.3.1
+
+./WPILibInstaller
